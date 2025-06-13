45aad7d279b13ad34d0f4292322d78ca85b739ad
Hiro Watanabe <hirwatan@c1r2s1.42tokyo.jp>
2025-06-13 19:43:52 +0900

    add ex00

--- cpp08/ex00/easyfind.hpp
+++ cpp08/ex00/easyfind.hpp
@@ -6,17 +6,17 @@
 /*   By: hirwatan <hirwatan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/12 19:13:49 by hirwatan          #+#    #+#             */
-/*   Updated: 2025/06/12 19:36:30 by hirwatan         ###   ########.fr       */
+/*   Updated: 2025/06/13 19:36:02 by hirwatan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EASYFIND_HPP
 #define EASYFIND_HPP
 
-#include <iostream>
 #include <algorithm>
-#include <vector>
-#include <list>
+#include <stdexcept>
+#include <iterator>
+#include <iostream>
 
 template <typename T>
 void easyfind(T &container, int value)
--- cpp08/ex00/main.cpp
+++ cpp08/ex00/main.cpp
@@ -6,15 +6,34 @@
 /*   By: hirwatan <hirwatan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/12 19:13:36 by hirwatan          #+#    #+#             */
-/*   Updated: 2025/06/12 19:26:03 by hirwatan         ###   ########.fr       */
+/*   Updated: 2025/06/13 19:43:19 by hirwatan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "easyfind.hpp"
+#include <vector>
+
+void TEST_FOUND(void)
+{
+    std::vector<int> vec;
+    vec.push_back(10);
+    vec.push_back(20);
+    vec.push_back(30);
+    easyfind(vec, 20);
+}
+
+void TEST_NOT_FOUND(void)
+{
+    std::vector<int> vec;
+    vec.push_back(10);
+    vec.push_back(20);
+    vec.push_back(30);
+    easyfind(vec, 50);
+}
 
 int main(void)
 {
-    int nums[] = {1, 2, 3, 4, 5};
-    easyfind(nums, 3);
+    TEST_FOUND();
+    TEST_NOT_FOUND();
     return 0;
 }
