e67a827bf0b4ed332e6c8e87329543e886b8c124
mels wonder <melswonder@melsnoMacBook-Air.local>
2025-06-16 11:00:44 +0900

    add cpp08

--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -3,29 +3,40 @@
 /*                                                        :::      ::::::::   */
 /*   MutantStack.hpp                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hirwatan <hirwatan@student.42.fr>          +#+  +:+       +#+        */
+/*   By: hirwatan <hirwatan@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/14 12:13:12 by hirwatan          #+#    #+#             */
-/*   Updated: 2025/06/15 19:34:25 by hirwatan         ###   ########.fr       */
+/*   Updated: 2025/06/16 11:00:17 by hirwatan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MUTANTSTACK
-#define MUTANTSTACK
+#ifndef MUTANTSTACK_HPP
+#define MUTANTSTACK_HPP
 
+#include <stack>
+#include <deque>
 
-template <typename T>
-class MutantStack
+template <typename T, typename Container = std::deque<T> >
+class MutantStack : public std::stack<T, Container>
 {
-    private:
-        MutantStack();
-    public:
-        MutantStack(new (T));
-        
-        void push(int num);
-        void pop(int num);
-        void top(int num);
-        void size(int num);
-}; 
+public:
+    typedef typename Container::iterator iterator;
+    typedef typename Container::const_iterator const_iterator;
 
+    MutantStack() : std::stack<T, Container>() {}
+    MutantStack(const MutantStack &src) : std::stack<T, Container>(src) {}
+    ~MutantStack() {}
+
+    MutantStack &operator=(const MutantStack &rhs)
+    {
+        if (this != &rhs)
+            std::stack<T, Container>::operator=(rhs);
+        return *this;
+    }
+
+    iterator begin() { return this->c.begin(); }
+    iterator end() { return this->c.end(); }
+    const_iterator begin() const { return this->c.begin(); }
+    const_iterator end() const { return this->c.end(); }
+};
 #endif
--- cpp08/ex02/main.cpp
+++ cpp08/ex02/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hirwatan <hirwatan@student.42.fr>          +#+  +:+       +#+        */
+/*   By: hirwatan <hirwatan@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/12 19:13:36 by hirwatan          #+#    #+#             */
-/*   Updated: 2025/06/15 19:34:18 by hirwatan         ###   ########.fr       */
+/*   Updated: 2025/06/16 10:56:57 by hirwatan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,18 @@
 int main()
 {
     MutantStack<int> mstack;
-    mstack.push(5);
-    mstack.push(17);
+    mstack.push(1);
+    mstack.push(2);
+    std::cout << "===top===" << std::endl;
     std::cout << mstack.top() << std::endl;
     mstack.pop();
+    std::cout << "===size===" << std::endl;
     std::cout << mstack.size() << std::endl;
     mstack.push(3);
+    mstack.push(4);
     mstack.push(5);
-    mstack.push(737);
     //[...]
-    mstack.push(0);
+    mstack.push(6);
     MutantStack<int>::iterator it = mstack.begin();
     MutantStack<int>::iterator ite = mstack.end();
     ++it;
